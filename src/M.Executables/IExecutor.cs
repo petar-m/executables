e08318b7a845ea7f6e4809fb754907b44b63e95e
petar <petar_m@mail.bg>
2017-07-10 19:45:31 +0300

    Added class constraint for executables.

--- src/M.Executables/IExecutor.cs
+++ src/M.Executables/IExecutor.cs
@@ -2,12 +2,12 @@
 {
     public interface IExecutor
     {
-        void Execute<TExecutableVoid>() where TExecutableVoid : IExecutableVoid;
+        void Execute<TExecutableVoid>() where TExecutableVoid : class, IExecutableVoid;
 
-        void Execute<TExecutableVoid, TInput>(TInput input) where TExecutableVoid : IExecutableVoid<TInput>;
+        void Execute<TExecutableVoid, TInput>(TInput input) where TExecutableVoid : class, IExecutableVoid<TInput>;
 
-        TResult Execute<TExecutable, TResult>() where TExecutable : IExecutable<TResult>;
+        TResult Execute<TExecutable, TResult>() where TExecutable : class, IExecutable<TResult>;
 
-        TResult Execute<TExecutable, TInput, TResult>(TInput input) where TExecutable : IExecutable<TInput, TResult>;
+        TResult Execute<TExecutable, TInput, TResult>(TInput input) where TExecutable : class, IExecutable<TInput, TResult>;
     }
 }
--- src/M.Executables/IExecutorAsync.cs
+++ src/M.Executables/IExecutorAsync.cs
@@ -4,12 +4,12 @@ namespace M.Executables
 {
     public interface IExecutorAsync
     {
-        Task ExecuteAsync<TExecutableVoid>() where TExecutableVoid : IExecutableVoid;
+        Task ExecuteAsync<TExecutableVoid>() where TExecutableVoid : class, IExecutableVoid;
 
-        Task ExecuteAsync<TExecutableVoid, TInput>(TInput input) where TExecutableVoid : IExecutableVoid<TInput>;
+        Task ExecuteAsync<TExecutableVoid, TInput>(TInput input) where TExecutableVoid : class, IExecutableVoid<TInput>;
 
-        Task<TResult> ExecuteAsync<TExecutable, TResult>() where TExecutable : IExecutable<TResult>;
+        Task<TResult> ExecuteAsync<TExecutable, TResult>() where TExecutable : class, IExecutable<TResult>;
 
-        Task<TResult> ExecuteAsync<TExecutable, TInput, TResult>(TInput input) where TExecutable : IExecutable<TInput, TResult>;
+        Task<TResult> ExecuteAsync<TExecutable, TInput, TResult>(TInput input) where TExecutable : class, IExecutable<TInput, TResult>;
     }
 }
